e7394cc9d6addb1998417a9812a4324e756684d9
pooneh <poonehshooshtari@gmail.com>
2017-11-12 06:51:29 +0330

    Initial commit Drag-Drop folder

---
+++ Drag-Drop/script.js
@@ -0,0 +1,16 @@
+console.log("are you here Pooneh?");
+
+function allowDrop(ev){
+	ev.preventDefault();	
+}
+
+
+function drag(ev){
+	ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev){
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+	ev.target.appendChild(document.getElementById(data));
+}
