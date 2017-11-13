c3207437f30f326102d5534ad37ff2c3a8ea98ff
pooneh <poonehshooshtari@gmail.com>
2017-11-13 09:58:00 +0330

    Added more features for thumbnails picture

--- src/script.js
+++ src/script.js
@@ -1,25 +1,26 @@
 // the code is from https://codepen.io/doughensel/pen/zGMmop
 
 var dragdrop = {};
+var total_image_num = 0;
 $(document).ready(function(){
 
 	'use strict';
-	// (function(){
-	// // http://stackoverflow.com/questions/4083351/what-does-jquery-fn-mean
-	// 	var $ = function( elem ){
-	// 		if (!(this instanceof $)){
-	//       return new $(elem);
-	// 		}
-	// 		this.el = document.getElementById( elem );
-	// 	};
-	// 	window.$ = $;
-	// 	$.prototype = {
-	// 		onChange : function( callback ){
-	// 			this.el.addEventListener('change', callback );
-	// 			return this;
-	// 		}
-	// 	};
-	// })();
+	(function(){
+	// http://stackoverflow.com/questions/4083351/what-does-jquery-fn-mean
+		var select = function( elem ){
+			if (!(this instanceof select)){
+	      return new select(elem);
+			}
+			this.el = document.getElementById( elem );
+		};
+		window.select = select;
+		select.prototype = {
+			onChange : function( callback ){
+				this.el.addEventListener('change', callback );
+				return this;
+			}
+		};
+	})();
 
 	// Drag and Drop code for Upload
 	dragdrop = {
@@ -34,12 +35,17 @@ $(document).ready(function(){
 			// and runUpload the files one by one
 			
 			var file = e.dataTransfer.files;
+			var previous_image_num = total_image_num;
+			total_image_num += file.length;
+			if (total_image_num > 5){
+					console.error("you cannot upload more than five pictures");
+					return;
+				}
 			for (var i=0; i<file.length; i++){
 				var myDiv = $('<div></div>');
-				var img = $(runUpload(file[i]));
-				console.log(myDiv);
-				myDiv.append(img);
-				$('body').appendChild(myDiv);	
+				runUpload(file[i], myDiv);
+				myDiv.css({"left":`${105 * (previous_image_num+i) }px`, "position":"absolute"});
+				$('body').append(myDiv);	
 			}
 
 		},
@@ -51,19 +57,19 @@ $(document).ready(function(){
 	};
 
 	// Code to capture a file(image) and upload it to the browser
-	function runUpload(file){
+	function runUpload(file, myDiv){
 		if( file.type === 'image/png' ||
 			file.type === 'image/jpg' ||
 			file.type === 'image/jpeg'||
 			file.type === 'image/gif' ||
 			file.type === 'image/bmp' ){
 		  var reader = new FileReader();
-		  var image = new Image();
 		  reader.readAsDataURL( file );
 		  // customizing the onload method and how to do onload
 		  reader.onload = function(file_being_loaded){
-		  	$('droppedImage').el.src = file_being_loaded.target.result;
-		  	$('droppedImage').el.style.display = 'inline';
+		  	var myImageTag = $('<img>');
+			myImageTag.attr("src",file_being_loaded.target.result);	
+			myDiv.append(myImageTag);
 
 		  } // END reader.onload()
 		} // END test if file.type === image
@@ -74,18 +80,18 @@ $(document).ready(function(){
 	window.onload = function(){
 		if (window.FileReader){
 			// connect the DIV surrounding the file upload to HTML5 drag and drop calls
-			console.log($('droppingDiv'));
-			dragdrop.init( $('droppingDiv').el );
+			console.log(select('droppingDiv'));
+			dragdrop.init( select('droppingDiv').el );
 			// bind the input [type="file"] to the function runUpload()
-			$('fileUpload').onChange(function(){runUpload(this.files[0]);});
+			select('fileUpload').onChange(function(){runUpload(this.files[0]);});
 		}else{
 			// report error message if FileReader is unavialable
 			var p = document.createElement('p');
 			var msg = document.createElement('Sorry, your browser does not support FileReader.');
 			p.className = 'error';
 			p.appendChild( msg );
-			$(droppingDiv).el.innerHTML = '';
-			$(droppingDiv).el.appendChild( p );
+			select(droppingDiv).el.innerHTML = '';
+			select(droppingDiv).el.appendChild( p );
 		}
 
 	};
